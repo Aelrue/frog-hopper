6c54f9b1a905b78d5fe8cc5b0584ac7375611eb3
Aelrue <littlehoatzin@gmail.com>
2023-06-07 12:09:36 -0500

    first commit, pushing to github

---
+++ HopperScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HopperScript : MonoBehaviour
+{
+    // Start is called before the first frame update. For any code that will run as soon as script is enabled.
+    // Runs once
+    void Start()
+    {
+        gameObject.name = "Hopper";
+    }
+
+    // Update is called once per frame. Runs constantly while script is enabled
+    void Update()
+    {
+        
+    }
+}
